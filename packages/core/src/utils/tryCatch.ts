730e3b12b5593bba7467cac0da019460d1954e67
Tom Lanser <tom@devv.nl>
2025-01-30 23:30:12 +0100

    feat: Overloading instead of ugly types
    Signed-off-by: Tom Lanser <tom@devv.nl>

--- packages/core/src/utils/tryCatch.ts
+++ packages/core/src/utils/tryCatch.ts
@@ -1,21 +1,19 @@
 type Result<T> = { success: true; value: T } | { success: false; error: unknown }
 
-export function tryCatch<TReturn>(
-  fn: () => TReturn
-): TReturn extends Promise<infer T> ? Promise<Result<T>> : Result<TReturn> {
+export function tryCatch<T>(fn: () => T): Result<T>
+export function tryCatch<T>(fn: () => Promise<T>): Promise<Result<T>>
+export function tryCatch<T>(fn: () => T | Promise<T>): Result<T> | Promise<Result<T>> {
   try {
     const result = fn()
 
     if (result instanceof Promise) {
       return result
         .then((value) => ({ success: true, value }) as const)
-        .catch((error: unknown) => ({ success: false, error }) as const) as TReturn extends Promise<infer T>
-        ? Promise<Result<T>>
-        : never
+        .catch((error: unknown) => ({ success: false, error }) as const)
     }
 
-    return { success: true, value: result } as TReturn extends Promise<infer T> ? Promise<Result<T>> : Result<TReturn>
+    return { success: true, value: result }
   } catch (error) {
-    return { success: false, error } as TReturn extends Promise<infer T> ? Promise<Result<T>> : Result<TReturn>
+    return { success: false, error }
   }
 }
